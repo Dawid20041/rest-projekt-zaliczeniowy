5cb2cb45471883191e3b7a86b63e398dbae363f2
Dawid Żmudzki <zmudzda8267_aehit@students.vizja.pl>
2026-04-19 20:13:06 +0200

    Implement user management CRUD operations in userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,5 +9,41 @@ const register = async (req, res) => {
     res.status(err.status || 500).json({ error: err.message });
   }
 };
+exports.createUser = async (req, res) => {
+    try {
+        const user = await userService.createUser(req.body)
+        res.status(201).json(user)
+    } catch (err) {
+        res.status(400).json({ error: err.message })
+    }
+}
 
+exports.getAllUsers = async (req, res) => {
+    const users = await userService.getAllUsers()
+    res.json(users)
+}
+
+exports.getUser = async (req, res) => {
+    const user = await userService.getUser(req.params.id)
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" })
+    }
+
+    res.json(user)
+}
+
+exports.updateUser = async (req, res) => {
+    try {
+        const user = await userService.updateUser(req.params.id, req.body)
+        res.json(user)
+    } catch (err) {
+        res.status(400).json({ error: err.message })
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+    await userService.deleteUser(req.params.id)
+    res.status(204).send()
+}
 module.exports = { register };
