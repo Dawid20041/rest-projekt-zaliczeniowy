2e7bafa5f4709727effc2e1bdc98cad2089a2845
Dawid Żmudzki <zmudzda8267_aehit@students.vizja.pl>
2026-04-19 20:08:39 +0200

    Remove user management controllers, services, and routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,4 +4,14 @@ const { register } = require('../controllers/userController');
 
 router.post('/', register);
 
+const userController = require("../controllers/userController")
+
+router.post("/", userController.createUser)
+router.get("/", userController.getAllUsers)
+router.get("/:id", userController.getUser)
+router.put("/:id", userController.updateUser)
+router.delete("/:id", userController.deleteUser)
+
+module.exports = router
+
 module.exports = router;
