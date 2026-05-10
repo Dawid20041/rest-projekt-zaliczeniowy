d4a6c1c92ff8ee5042e2adf9bc00816a03729614
Dawid Żmudzki <zmudzda8267_aehit@students.vizja.pl>
2026-05-10 14:35:34 +0200

    refactor: rename auctionRoutes to aukcjaRoutes and remove unused files

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ const express = require("express")
 const app = express()
 
 const userRoutes = require("./routes/userRoutes")
-const auctionRoutes = require("./routes/auctionRoutes")
+const aukcjaRoutes = require("./routes/aukcjaRoutes")
 
 app.use(express.json())
 
 app.use("/user", userRoutes)
-app.use("/auctions", auctionRoutes)
+app.use("/auctions", aukcjaRoutes)
 
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000")
