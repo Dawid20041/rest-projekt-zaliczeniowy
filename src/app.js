cfd4a28ed77d7323275eca751210d78d4c6bdea6
Dawid20041 <dawid.zmudzki@wp.pl>
2026-05-13 09:09:42 +0200

    feat: integrate Swagger for API documentation and add user routes documentation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
+const { swaggerUi, swaggerSpec } = require("./config/swagger")
 const express = require("express")
 const app = express()
+
 
 const userRoutes = require("./routes/userRoutes")
 const aukcjaRoutes = require("./routes/aukcjaRoutes")
@@ -8,6 +10,7 @@ app.use(express.json())
 
 app.use("/user", userRoutes)
 app.use("/auctions", aukcjaRoutes)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000")
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,3 +1,4 @@
+
 const express = require("express")
 const router = express.Router()
 
@@ -10,3 +11,26 @@ router.put("/:id", userController.updateUser)
 router.delete("/:id", userController.deleteUser)
 
 module.exports = router
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Get all users
+ *     responses:
+ *       200:
+ *         description: List of users
+ */
+router.get("/", userController.getAllUsers)
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Create user
+ *     requestBody:
+ *       required: true
+ *     responses:
+ *       201:
+ *         description: User created
+ */
+router.post("/", userController.createUser)
